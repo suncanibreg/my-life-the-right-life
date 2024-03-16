247045a26bbfbd69cb38ce1db2880f3c9fadf06c
Milan Stojanovic <mlnstojanovic2010@gmail.com>
2024-03-16 22:07:36 +0100

    builded should resolve for succesfull vercel deployment

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,4 @@
-import Navbar1 from "./components/navbar";
+import Navbar1 from "./components/navbar/navbar";
 
 export default function Home() {
   return (
---
+++ src/app/psichologically/page.jsx
@@ -0,0 +1,28 @@
+import Navbar1 from "../components/navbar/navbar";
+
+export default function Home() {
+  return (
+    <div className=" m-0 p-0 w-full flex flex-col justify-center items-">
+      <div className="w-full">
+        <Navbar1 />
+      </div>
+
+      <p>
+        The website will be at first similar to blog posts and later will
+        develop to more complex one. Mostly I will wrote various psychological
+        based methods how to find the most present mistakes of modern man/woman
+        in personal life. Also, what attitudes and behaviors mostly helped to
+        various people or professionals worked with clients have written it
+        helped. But I do not want to limit myself just on psychology. Sure it is
+        the most important point. The start, the basic condition for someone to
+        successfully change his/her own life is to really learn about self. But
+        What about the way of life that push and smash modern man/woman. So, I
+        will write my and other, both professionals and people in general
+        experiences and opinions on that topic. What kind of life was, is or
+        would be the most fruitful for various people/families. What kind of
+        life environment, organization, work field helped the most to people to
+        feel better and to better grow up their own children.
+      </p>
+    </div>
+  );
+}
