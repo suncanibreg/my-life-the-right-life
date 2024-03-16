a59cfb729a57e27e370f1d30678bfdd21e5530d1
milan <mlnstojanovic2010@gmail.com>
2024-03-16 10:14:19 +0100

    first commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,28 @@
+export default function Home() {
+  return (
+    <>
+      <p>
+        The website will be at first similar to blog posts and later will
+        develop to more complex one. Mostly I will wrote various psichological
+        based methods how to find the most present misstakes of modern man/woman
+        in personal life. Also, what attitudes and behaviors mostly helped to
+        various people or professionals worked with clients have written it
+        helped. But I don't want to limit myself just on psichology. Sure it is
+        the most important point. The start, the basic condition for someone to
+        successfully change his/her own life is to really learn about self. But
+        What about the way of life that push and smash modern man/woman. So, I
+        will write my and other, both professionals and people in general
+        experiences and opinions on that topic. What kind of life was, is or
+        would be the most fruitful for various people/families. What kind of
+        life environment, organisation, work field helped the most to people to
+        feel better and to better grow up their own children.
+      </p>
+    </>
+  );
+}
+
+/* from the basic template-how to make various Home Page.jsx things?
+
 import Image from "next/image";
 
 export default function Home() {
@@ -111,3 +136,4 @@ export default function Home() {
     </main>
   );
 }
+*/
